e582ca023538ffcc11daee6559348aafce6c2594
Akinrinade Oluwatobi <37489048+thobby16@users.noreply.github.com>
2018-06-20 01:22:52 +0100

    create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+# Devotion-blog
+var express = require('express');
+var router = express.Router();
+var mongo = require('mongodb');
+var db = require('monk')('localhost/dailyblog');
+
+
+//Home page daily blog
+router.get('/', function(req, res, next) {
+  var db = req.db;
+  var posts = db.get('posts');
+  posts.find({},{}, function (err, posts) {
+    res.render('index',{
+      "posts": posts
+    });
+      
+  });
+});
+
+module.exports = router;
